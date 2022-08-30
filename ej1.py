c4b4c08645b82ded40f8a923d19d39ce0e09d954
SebastianNavarro <111942963+SebastianNavarro@users.noreply.github.com>
2022-08-30 09:29:58 -0500

    Add files via upload

--- ej1.py
+++ ej1.py
@@ -1,10 +1,7 @@
 def sum(n):
     suma=0
-    operaciones=0
     for n in range (1,n+1):
         suma=suma+n
-        operaciones=operaciones+1
-    print(operaciones)
     return suma
 
 print(sum(5))
