a58fec45eaa93b500781812657e36a52a02fb2f7
SebastianNavarro <sebitaswinter@hotmail.com>
2022-08-30 13:56:44 -0500

    Update ej1.py

--- class_exercise_1/ej1.py
+++ class_exercise_1/ej1.py
@@ -1,10 +1,8 @@
 def sum(n):
     suma=0
-    operaciones=0
     for n in range (1,n+1):
         suma=suma+n
-        operaciones=operaciones+1
-    print(operaciones)
     return suma
 
+print("El resultado es: ")
 print(sum(4))
